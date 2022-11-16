1743bf7cea64d6c4f72cc4d5a74fa6040b9ba80f
OutdatedGuy <everythingoutdated@gmail.com>
2022-11-16 20:44:53 +0530

    feat: added user registration frontend ui code (#21)
    * feat: added user registration fromtend ui code
    
    * feat: added pattern check for input fields

---
+++ public/signup-user/assets/js/api/registerUser.js
@@ -0,0 +1,44 @@
+"use strict";
+
+import { userInfo } from "../forms/userForm.js";
+import { addressInfo } from "../forms/addressForm.js";
+import { loginInfo } from "../forms/loginForm.js";
+
+const registerUser = async () => {
+  document.querySelector(".loadingContainer").classList.toggle("loading");
+
+  const data = {
+    ...userInfo,
+    ...addressInfo,
+    ...loginInfo,
+  };
+
+  const arg = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
+  };
+
+  try {
+    const response = await fetch("/api/registration/user", arg);
+    const result = await response.json();
+
+    if (result.code === 200) {
+      alert("User Registered Successfully...");
+      window.location.replace("../");
+    } else if (result.code === 500) {
+      throw new Error(result.message);
+    } else {
+      alert(result.message);
+    }
+  } catch (error) {
+    console.log(error);
+    alert("User Registration Failed!!!\nPlease try again later!");
+  }
+
+  document.querySelector(".loadingContainer").classList.toggle("loading");
+};
+
+export { registerUser };
---
+++ public/signup-user/assets/js/forms/loginForm.js
@@ -0,0 +1,39 @@
+"use strict";
+
+import { registerUser } from "../api/registerUser.js";
+import { prevStep } from "/assets/js/forms/progressBar.js";
+
+let loginInfo;
+
+const setLoginForm = () => {
+  const loginFormContainer = document.querySelector("#login-form");
+  const loginForm = document.querySelector("#loginForm");
+  const prevBtn = loginForm.querySelector(".prev-btn");
+
+  prevBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+
+    loginFormContainer.style.display = "none";
+    document.querySelector("#address-form").style.display = "block";
+
+    prevStep();
+  });
+
+  loginForm.addEventListener("submit", async (e) => {
+    e.preventDefault();
+
+    if (loginForm.password.value !== loginForm.confirmPassword.value) {
+      alert("Passwords do not match");
+      return;
+    }
+
+    loginInfo = {
+      userName: loginForm.userName.value,
+      password: loginForm.password.value,
+    };
+
+    await registerUser();
+  });
+};
+
+export { setLoginForm, loginInfo };
