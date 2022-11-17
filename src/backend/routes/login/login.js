d806d803eb669facad3261412d237f98120c478a
Vinayak Mali <malivinayak012@gmail.com>
2022-11-17 08:45:44 +0530

    Login API Route

--- src/backend/routes/login/login.js
+++ src/backend/routes/login/login.js
@@ -1,5 +1,18 @@
 import oracledb from 'oracledb';
 import { dbConfig } from '../../dbconfig.js';
+import fs from 'fs';
+import { randomBytes } from "crypto";
+
+let libPath;
+if (process.platform === 'win32') {           // Windows
+    libPath = 'C:\\oracle\\instantclient_19_12';
+} else if (process.platform === 'darwin') {   // macOS
+    libPath = process.env.HOME + '/Downloads/instantclient_19_8';
+}
+if (libPath && fs.existsSync(libPath)) {
+    oracledb.initOracleClient({ libDir: libPath });
+}
+
 
 const login = async (req, res) => {
     // console.log(req.body);
@@ -7,7 +20,7 @@ const login = async (req, res) => {
 
     try {
         const { userName, password } = req.body;
-        const role = req.params.role;
+        let role = req.params.role;
 
         if (!userName || !password || !role) {
             return res.status(403).send({
@@ -23,45 +36,71 @@ const login = async (req, res) => {
             });
         }
 
+        console.log(role);
+        role = role == "user" ? "CLIENT" : role == "driver" ? "EMPLOYEE" : "ADMIN";
+        console.log(role);
+
+        let connection, query, options, result;
         // query
         try {
             // DB Connection
             connection = await oracledb.getConnection(dbConfig);
+            options = {
+                outFormat: oracledb.OUT_FORMAT_OBJECT,   // query result format
+            };
 
-            const results = await connection.execute(
-                // The statement to execute
-                `SELECT *
-                FROM :role
-                where userName  = :userName`,
-                [role],
-                [userName],
-                {
-                    maxRows: 1
-                });
+            console.log("1");
+            // query = `SELECT * FROM :1 where userName = :2`;
+            // const results = await connection.execute(query, [role, userName], options);
 
-            if (results.length === 0) {
+            if (role === "CLIENT") {
+                query = `select * from TRIP_MANAGEMENT_SYSTEM.client where USERNAME = :1`
+            } else if (role === "EMPLOYEE") {
+                query = `select * from TRIP_MANAGEMENT_SYSTEM.EMPLOYEE where USERNAME = :1`;
+            } else {
+                query = `select * from TRIP_MANAGEMENT_SYSTEM.ADMIN where USERNAME = :1`;
+            }
+
+
+            const results = await connection.execute(query, [userName], options);
+
+            console.log("2");
+            if (results.rows[0] === undefined) {
                 return res.send({
                     status: "failure",
                     message: "This Username does NOT Exists!!!",
                     code: 400,
                 });
-            } else if (results[0].token !== null) {
+            } else if (results.rows[0].token !== null) {
+                const restTokenQuery = `UPDATE :1 SET TOKEN = :2 WHERE USERID = :3;`;
+                await connection.execute(restTokenQuery, [role, null, userName], options);
+
                 return res.send({
                     status: "failure",
-                    message: "Already logged in!!!\nTry After logout from other device",
+                    message: "Already logged in device detected!!!\nRefresh the page and login again",
                     code: 400,
                 });
-            } else if (results[0].password !== password) {
+            } else if (results.rows[0].password !== password) {
                 return res.send({
                     status: "failure",
                     message: "Invalid Credentials!!!",
                     code: 400,
                 });
+            } else {
+                const token = randomBytes(8).toString("hex");
+                const restTokenQuery = `UPDATE :1 SET TOKEN = :2 WHERE USERID = :3;`;
+                await connection.execute(restTokenQuery, [role, token, userName], options);
+
+                return res.send({
+                    status: "success",
+                    code: 200,
+                    data: {
+                        token: token,
+                    },
+                });
             }
-
-
-
         }
+
         catch (err) {
             console.log(" Error at Data Base : " + err);
             return res.status(500).send({
