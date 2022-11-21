94ff4df0aae118fb3b7e439aa29b39bafbbe5aa2
OutdatedGuy <everythingoutdated@gmail.com>
2022-11-21 23:22:00 +0530

    feat: admin dashboard ui with user query form

---
+++ public/admin/assets/js/index.js
@@ -0,0 +1,7 @@
+import { setUserQueryForm } from "./forms/setUserQueryForm.js";
+import { setTable } from "./DataTable---Fully-BSS-Editable.js";
+
+$(document).ready(() => {
+  setUserQueryForm();
+  setTable();
+});
