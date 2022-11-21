d0159328a215284c75616bcd96e6a3c380a71e83
OutdatedGuy <everythingoutdated@gmail.com>
2022-11-21 23:50:40 +0530

    feat: added driver query form to admin dashboard

--- public/admin/assets/js/index.js
+++ public/admin/assets/js/index.js
@@ -1,7 +1,9 @@
+import { setDriverQueryForm } from "./forms/setDriverQueryForm.js";
 import { setUserQueryForm } from "./forms/setUserQueryForm.js";
 import { setTable } from "./DataTable---Fully-BSS-Editable.js";
 
 $(document).ready(() => {
+  setDriverQueryForm();
   setUserQueryForm();
   setTable();
 });
