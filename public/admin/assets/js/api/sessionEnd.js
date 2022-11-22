875ff9556ae1300a29a118c20e29f7bbc312e011
OutdatedGuy <everythingoutdated@gmail.com>
2022-11-22 23:38:39 +0530

    fix: admin session token not expire on page reload

--- public/admin/assets/js/api/sessionEnd.js
+++ public/admin/assets/js/api/sessionEnd.js
@@ -3,6 +3,7 @@
 window.onbeforeunload = function () {
   // Get token from sessionStorage
   const token = sessionStorage.getItem("token");
+  sessionStorage.removeItem("token");
 
   fetch("/api/session-end/admin", {
     method: "POST",
