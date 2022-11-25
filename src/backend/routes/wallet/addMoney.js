eea3f15666f83181ff13f3a01858fb87e68e6373
Vinayak Mali <66154908+malivinayak@users.noreply.github.com>
2022-11-25 19:09:59 +0530

    API Route: add money (#75)
    * fix: add-money cbs route
    
    * API addMoney
    
    * removing extra lines
    
    * setting minimum deposit amount to 10
    
    * setting maximum limit
    
    * updating minimum amount to 100
    
    * standardizing the error message

---
+++ src/backend/routes/wallet/addMoney.js
@@ -0,0 +1,97 @@
+import oracledb from 'oracledb';
+import { dbConfig } from '../../dbconfig.js';
+import fs from 'fs';
+
+let libPath;
+if (process.platform === 'win32') {           // Windows
+    libPath = 'C:\\oracle\\instantclient_19_12';
+} else if (process.platform === 'darwin') {   // macOS
+    libPath = process.env.HOME + '/Downloads/instantclient_19_8';
+}
+if (libPath && fs.existsSync(libPath)) {
+    oracledb.initOracleClient({ libDir: libPath });
+}
+
+const addMoney = async (req, res) => {
+
+    try {
+        const { token } = req.body;
+        let { amount } = req.body;
+        amount = parseInt(amount)
+        if (amount < 100) {
+            return res.status(403).send({
+                status: "failure",
+                message: "Minimum amount must be 100!!!",
+                code: 407,
+            });
+        } else if (amount > 10000) {
+            return res.status(403).send({
+                status: "failure",
+                message: "You can not deposit more than 10k in single transaction!!!",
+                code: 407,
+            });
+        }
+        if (!token || !amount) {
+            return res.status(403).send({
+                status: "failure",
+                message: "Token or amount not received!!!",
+                code: 403,
+            });
+        }
+
+        let connection, query, options;
+        try {
+            connection = await oracledb.getConnection(dbConfig);
+
+            options = {
+                outFormat: oracledb.OUT_FORMAT_OBJECT,
+            };
+
+            const getBalQuery = `select WALLET_BALANCE from TRIP_MANAGEMENT_SYSTEM.client where TOKEN = :1`;
+            const getCurrentBalance = await connection.execute(getBalQuery, [token], options);
+            if (getCurrentBalance.rows[0] === undefined) {
+                return res.status(403).send({
+                    message: "Something went wrong!!!\nplease refresh the page and retry",
+                    status: "failure",
+                    code: 400,
+                });
+            }
+            let currentBalance = getCurrentBalance.rows[0].WALLET_BALANCE;
+            currentBalance = currentBalance + amount;
+
+            query = `update TRIP_MANAGEMENT_SYSTEM.client SET WALLET_BALANCE = ${currentBalance} where TOKEN = '${token}'`;
+            await connection.execute(query, [], { autoCommit: true });
+            return res.send({
+                message: `🎉 Balance Deposited success.......
+To Check updated balance click "Get Wallet Balance" button again`,
+                status: "success",
+                code: 200,
+            });
+
+        } catch (err) {
+            console.log(" Error at Data Base : " + err);
+            return res.status(500).send({
+                message: "Balance Deposit Failed!!!\nPlease try again later",
+                status: "failure",
+                code: 500,
+            });
+        } finally {
+            if (connection) {
+                try {
+                    await connection.close();
+                } catch (err) {
+                    console.error("Connection Close Error :" + err);
+                }
+            }
+        }
+    } catch (err) {
+        console.log(err);
+        return res.status(500).send({
+            message: "Something went Wrong!!!",
+            status: "failure",
+            code: 500,
+        });
+    }
+}
+
+export { addMoney };
